c04a15c636540ec84688e50335a2ef3dc81cc31c
Ahmed-Baha <ahmedbahaa948@gmail.com>
2024-12-07 15:01:25 +0200

    ad buton

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -11,6 +11,7 @@ export default function TodoForm(){
             <Input placeholder="Input component" value={input} onChange={(e)=>setInput(e.target.value)} />
             <Button onClick={()=>addTask()}>add</Button>
             <Button>Clear</Button>
+            <Button>Clear</Button>
         </div>
         </div>
     )
